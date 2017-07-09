55feca71f9694859297bd38641350aa632cd804e
Cooper Green <csgreen3@wisc.edu>
2017-07-09 18:09:13 -0500

    clean up more put board init in nuc144.c

--- common/nuc144.c
+++ common/nuc144.c
@@ -1,5 +1,36 @@
-#include "nuc144.h"
+#include "main.h"
 
+static void CPU_CACHE_Enable();
+static void SystemClock_Config();
+int initUART(void);
+
+static void Error_Handler(void) {
+	/* Turn LED3 on */
+	BSP_LED_On(LED_RED);
+	while (1) { }
+
+}
+
+int board_init(void) {
+	CPU_CACHE_Enable();
+	HAL_Init();
+	SystemClock_Config();
+
+	/* TODO: setup oscillator settings using HAL RCC */
+/*	SystemCoreClockUpdate();*/
+/*	SysTick_Config(SystemCoreClock / 1000);*/
+
+	BSP_LED_Init(LED_GREEN);
+	BSP_LED_Init(LED_BLUE);
+	BSP_LED_Init(LED_RED);
+
+	BSP_PB_Init(BUTTON_USER, BUTTON_MODE_GPIO);
+
+	initUART();
+	printf("uart init\r\n");
+
+	return 0;
+}
 /*****************************************************************************/
 /*                        				LED		                             */
 /*****************************************************************************/
@@ -95,3 +126,78 @@ uint32_t BSP_PB_GetState(Button_TypeDef Button)
 {
   return HAL_GPIO_ReadPin(BUTTON_PORT[Button], BUTTON_PIN[Button]);
 }
+/*****************************************************************************/
+/*                   	 	  		  UART		                             */
+/*****************************************************************************/
+int initUART() {
+
+	UartHandle.Instance        = USARTx;
+	
+	UartHandle.Init.BaudRate   = 115200;
+	UartHandle.Init.WordLength = UART_WORDLENGTH_8B;
+	UartHandle.Init.StopBits   = UART_STOPBITS_1;
+	UartHandle.Init.Parity     = UART_PARITY_NONE;
+	UartHandle.Init.HwFlowCtl  = UART_HWCONTROL_NONE;
+	UartHandle.Init.Mode       = UART_MODE_TX_RX;
+	UartHandle.Init.OverSampling = UART_OVERSAMPLING_16;
+
+	if (HAL_UART_Init(&UartHandle) != HAL_OK) {
+		/* Initialization Error */
+		Error_Handler();
+	}
+	return 0;
+}
+
+/*****************************************************************************/
+/*                        			CLOCK		                             */
+/*****************************************************************************/
+void SystemClock_Config(void) {
+	RCC_ClkInitTypeDef RCC_ClkInitStruct;
+	RCC_OscInitTypeDef RCC_OscInitStruct;
+
+	/* Enable HSE Oscillator and activate PLL with HSE as source */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
+	RCC_OscInitStruct.HSIState = RCC_HSI_OFF;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+	RCC_OscInitStruct.PLL.PLLM = 8;
+	RCC_OscInitStruct.PLL.PLLN = 432;  
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+	RCC_OscInitStruct.PLL.PLLQ = 9;
+	RCC_OscInitStruct.PLL.PLLR = 7;
+
+	if(HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+		while(1) {};
+	}
+	/* Activate the OverDrive to reach the 216 Mhz Frequency */
+
+	if(HAL_PWREx_EnableOverDrive() != HAL_OK) {
+		while(1) {};
+	}
+
+	/* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 
+	   clocks dividers */
+	RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;  
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;  
+	if(HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_7) != HAL_OK)
+	{
+		while(1) {};
+	}
+}
+
+
+/*****************************************************************************/
+/*                       			CACHE		                             */
+/*****************************************************************************/
+static void CPU_CACHE_Enable(void){
+	/* Enable I-Cache */
+	SCB_EnableICache();
+	/* Enable D-Cache */
+	SCB_EnableDCache();
+
+}
+
